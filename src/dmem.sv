1b5e9758f1d2f600c315a7615b1dc65a72e16434
hoseony <hoseon.alex.yu@gmail.com>
2026-04-24 20:51:26 -0400

    adder, dff, imem, dmem(wip)

---
+++ src/dmem.sv
@@ -0,0 +1,13 @@
+// =======================================================
+// ECE251B - Computer Architecture
+// Prof. Rob Marano
+// Author: Hoseon Yu & Evan Dong
+//
+// data memory
+// =======================================================
+
+`ifndef ALU
+`define ALU
+
+`timescale 1ns/100ps
+
